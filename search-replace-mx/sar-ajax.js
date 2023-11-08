6773189cb13c2f28a50cddb437e6a3da6f723818
MaksWP <qashqai911@gmail.com>
2023-11-08 18:41:55 +0200

    first

---
+++ search-replace-mx/sar-ajax.js
@@ -0,0 +1,18 @@
+jQuery(document).ready(function($) {
+    $('#search-form').on('submit', function(e) {
+        e.preventDefault();
+        var search_term = $('#search-term').val();
+
+        $.ajax({
+            url: sar_ajax_object.ajax_url,
+            type: 'post',
+            data: {
+                action: 'sar_search_posts',
+                search_term: search_term
+            },
+            success: function(response) {
+                $('#search-results').html(response);
+            }
+        });
+    });
+});
